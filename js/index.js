631e85034fdb66c803db6f1343bdbf9debcbf0c5
vlearner <barotvishal12@outlook.com>
2019-07-07 23:18:47 -0500

    index.js created

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+
+    setTimeout(on, 3000);
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+
+}
