dc0f11e06f554bf297ac19c9dc1eaf2b37715102
vlearner <barotvishal12@outlook.com>
2019-07-20 21:11:24 -0500

    Project file clean

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+init ()
+   
+   
    function on() {
         document.getElementById("overlay").style.display = "block";
 
@@ -7,4 +10,26 @@
     function off() {
         document.getElementById("overlay").style.display = "none";
 
+    }
+
+    function enableButton(){
+        document.getElementById("teamName").disabled = true;
+    }
+
+    document.querySelector()
+
+
+    function init (){
+
+        // Team name
+        document.getElementById('Team1').textContent = 'Team 1';
+        document.getElementById('Team2').textContent = 'Team 2';
+        
+        //Team score
+        document.getElementById('Score1').textContent = '0';
+        document.getElementById('Score2').textContent = '0';
+
+        //Team current score
+        document.getElementById('current1').textContent = '0';
+        document.getElementById('current2').textContent = '0';
     }
