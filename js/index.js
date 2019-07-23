9ffcb98742500924601922bcc8fbbb5e9ea9e247
vlearner <barotvishal12@outlook.com>
2019-07-22 19:22:08 -0500

    Score add is Working

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
+var addScore, deductScore, finalScore1, finalScore2
 init ()
    
+   document.querySelector(".btnPlus1").addEventListener('click', function(){
+    addScore = 10;
+    finalScore1 += addScore;
+    document.querySelector('#Score1').textContent = finalScore1;
+   })
    
+   document.querySelector(".btnPlus2").addEventListener('click', function(){
+    addScore = 10;
+    finalScore2 += addScore;
+    document.querySelector('#Score2').textContent = finalScore2;
+   })
+
    function on() {
         document.getElementById("overlay").style.display = "block";
 
@@ -16,10 +28,10 @@ init ()
         document.getElementById("teamName").disabled = true;
     }
 
-    document.querySelector()
-
-
     function init (){
+
+        finalScore1 = 0;
+        finalScore2 = 0;
 
         // Team name
         document.getElementById('Team1').textContent = 'Team 1';
@@ -29,7 +41,4 @@ init ()
         document.getElementById('Score1').textContent = '0';
         document.getElementById('Score2').textContent = '0';
 
-        //Team current score
-        document.getElementById('current1').textContent = '0';
-        document.getElementById('current2').textContent = '0';
     }
